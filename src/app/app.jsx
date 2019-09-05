4c2dba508f30e53bc67e518fb00a164d4d71009a
Andrew Tate <drewctate@gmail.com>
2019-09-04 22:06:16 -0600

    More Sass

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -3,7 +3,7 @@ import './app.scss';
 
 class App extends Component {
     render() {
-        return <div><h1>Preact Chrome Extension Starter</h1></div>
+        return <div id="app-root"><h1>Preact Chrome Extension Starter</h1></div>
     }
 }
 
